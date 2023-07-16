a0fde8af761de9327d9c295bbadd797897bf4b5c
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-07-16 13:15:54 +0300

    update log

--- src/main/java/com/example/Info_ExchangeBot/service/utilits/Log.java
+++ src/main/java/com/example/Info_ExchangeBot/service/utilits/Log.java
@@ -1,24 +1,40 @@
 package com.example.Info_ExchangeBot.service.utilits;
 
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Timestamp;
 
 public class Log {
 
+   private static final File FILE = new File("log.txt");
+
     public static void Error(TelegramApiException e) {
-        System.out.println("LOG.ERROR: " + new Timestamp(System.currentTimeMillis()) +
+        System.out.println("\nLOG.ERROR: " + new Timestamp(System.currentTimeMillis()) +
                 "\nError occurred: " + e.getMessage() + "\n");
     }
 
     public static void Info(String name, String text) {
         System.out.println("\nLOG.INFO: " + new Timestamp(System.currentTimeMillis()) +
                 "\nReply to username: " + name + "\n" +
-                "using text: " + text);
+                "using text: " + text + "\n");
     }
 
     public static void button(String name, String callbackData) {
-        System.out.println("\nLOG.INFO: " + new Timestamp(System.currentTimeMillis()) +
+        String logText = "\nLOG.INFO: " + new Timestamp(System.currentTimeMillis()) +
                 "\nReply to username: " + name +  "\n" +
-                "using button: " + callbackData);
+                "using button: " + callbackData +  "\n";
+
+        System.out.println(logText);
+
+        try (FileWriter writer = new FileWriter(FILE, true))
+        {
+            writer.write(logText);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
